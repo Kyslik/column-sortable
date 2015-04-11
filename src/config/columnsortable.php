7141b084da8ed615e049f283a348fbc7af06f88c
cinject <admin@santona.ru>
2015-04-11 15:26:35 +0300

    Fix url params and reorganize config file

--- src/config/columnsortable.php
+++ src/config/columnsortable.php
@@ -2,58 +2,23 @@
 
 return [
 
-
     /*
-    Array that defines columns with numerical data. 
-    These columns are going to have 'numeric_icon_set' next to sortable link.
+    Spec columns
     */
-    'numeric_columns'  => ['created_at',
-                           'updated_at',
-                           'level',
-                           'id'],
-
-
-    /*
-    Array that defines columns with amount data. 
-    These columns are going to have 'amount_icon_set' next to sortable link.
-    */
-    'amount_columns'   => [],
-
-
-    /*
-    Array that defines columns with amount data. 
-    These columns are going to have 'alpha_icon_set' next to sortable link.
-    */
-    'alpha_columns'    => ['name',
-                           'descrtiption',
-                           'email',
-                           'slug'],
-
-
-    /*
-    Before implementing your own classes (not Font Awesome),
-    note that full class names are ending with either 'asc' or 'desc'
-    e.g.: 'fa fa-sort-numeric-desc'. Order (asc|desc) is appended automatically.
-    */
-
-
-    /*
-    Defines icon set to use when sorted data is numerical.
-    */
-    'numeric_icon_set' => 'fa fa-sort-numeric',
-
-
-    /*
-    Defines icon set to use when sorted data is amount.
-    */
-    'amount_icon_set'  => 'fa fa-sort-amount',
-
-
-    /*
-    Defines icon set to use when sorted data is alphabetical.
-    */
-    'alpha_icon_set'   => 'fa fa-sort-alpha',
-
+    'columns' => [
+        'numeric_columns'  => [
+            'rows' => ['created_at', 'updated_at', 'level', 'id'],
+            'class' => 'fa fa-sort-numeric'
+        ],
+        'amount_columns'   => [
+            'rows' => ['price'],
+            'class' => 'fa fa-sort-amount'
+        ],
+        'alpha_columns'    => [
+            'rows' => ['name', 'description', 'email', 'slug'],
+            'class' => 'fa fa-sort-alpha',
+        ],
+    ],
 
     /*
     Defines icon set to use when sorted data is none above.
@@ -63,6 +28,6 @@ return [
     /*
     Icon that shows when generating sortable link while column is not sorted.
     */
-    'sortable_icon'    => 'fa fa-sort' 
+    'sortable_icon'    => 'fa fa-sort'
 
 ];
