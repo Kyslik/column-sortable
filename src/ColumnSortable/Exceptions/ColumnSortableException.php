4c7b7e639afc4ff4d2aa863ed6a971ec5aabc4e1
Martin Kiesel <martin.kiesel@gmail.com>
2016-08-27 12:56:12 +0200

    changing Exception if-else to switch statement; changing Exception messages

--- src/ColumnSortable/Exceptions/ColumnSortableException.php
+++ src/ColumnSortable/Exceptions/ColumnSortableException.php
@@ -8,12 +8,16 @@ class ColumnSortableException extends Exception
 {
     public function __construct($message = '', $code = 0, Exception $previous = null)
     {
-        if ($code === 1) {
-            $message = 'Relation \''. $message . '\' does not exist. Define it in your model.';
-        } elseif ($code === 2) {
-            $message = 'Relation \'' . $message . '\' is not instance of HasOne, HasMany or BelongsTo.';
-        } else {
-            $message = 'Invalid sort argument, explode() did not produce array with size of 2 elements.';
+        switch ($code) {
+            case 0:
+                $message = 'Invalid sort argument.';
+                break;
+            case 1:
+                $message = 'Relation \''. $message . '\' does not exist.';
+                break;
+            case 2:
+                $message = 'Relation \'' . $message . '\' is not instance of HasOne, HasMany or BelongsTo.';
+                break;
         }
 
         parent::__construct($message, $code, $previous);
